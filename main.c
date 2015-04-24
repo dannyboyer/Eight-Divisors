fbe6da3c5f2aac86562264569b7cc55fe2dfe8ed
dannyboyer <dannyboyer49@gmail.com>
2015-04-24 16:21:38 -0400

    init

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+   unsigned int max = 100;
+   unsigned int count = 0;
+   unsigned int i = 0;
+ 
+   //iterate between min to max
+   
+   //check if current number has height divisors. Actually, check for 7 because a number will always has 1 and itself as divisors
+  
+   //divisor count = 2; 
+   
+   
+
+   //if yes, increment count
+   
+   
+   
+    printf("yeah!\n");
+
+    return 0;    
+}
