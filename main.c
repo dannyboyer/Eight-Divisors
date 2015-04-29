061d68a8487b7e8cbbc87c712ef611f288fafb87
dannyboyer <dannyboyer49@gmail.com>
2015-04-28 22:09:40 -0400

    first working solution

--- main.c
+++ main.c
@@ -3,35 +3,37 @@
 
 int main(int argc, char *argv[])
 {
-    unsigned int max = 100;
+    //arg
+    unsigned int max = 1000000;
    
     unsigned int count, i, div_count, divisor;
     
     //iterate between min to max
     //optimize loop by going 100 to 1 instead of the inverse
-    for(i = max; i > 0; i--)
+    for(i = 1; i <= max; i++)
     {
         //check if current number has height divisors. Actually, check for 6 because a number will always has 1 and itself as divisors
         div_count = 0;
-        divisor = 2;
+        divisor = 1;
 
-        while(divisor < i)
+        while(divisor <= i)
         {
             if (i % divisor == 0){
-                div_count += 1;
+                div_count += 1; 
             }
             
-            if(div_count == 6)
-            {
-                count += 1;    
-                printf("%d\n", i);
-            }
             
             divisor += 1;    
         }
+        
+        if(div_count == 8)
+        {
+            count += 1;    
+            printf("divisor : %d\n", i);
+        }
     }
 
-    printf("%d\n", count);
+    printf("count : %d\n", count);
    
     return 0;    
 }
